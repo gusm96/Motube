eafdcec3636fe5bd219779456a94782a2486a4e1
Seongmo Gu <gusm96@naver.com>
2021-05-12 23:18:00 +0900

    21.05.12

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,8 @@
-import video from "../models/Video";
+import Video from "../models/Video";
 
-export const getHome = (req, res) => {
-  return res.render("home", { pageName: "Home" });
+export const getHome = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageName: "Home", videos });
 };
 export const getWatch = (req, res) => {
   const { id } = req.params;
@@ -19,7 +20,19 @@ export const postEditVideo = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageName: "Upload Vidoe" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
+export const postUpload = async (req, res) => {
+  const { title, description, hashtag } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtag: hashtag.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageName: "Uplaod Video",
+      errorMessage: error._message,
+    });
+  }
 };
