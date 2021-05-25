0991a38d908b61e63cc568006172af386cad5aba
Seongmo Gu <gusm96@naver.com>
2021-05-25 17:52:18 +0900

    21.05.25 update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import sesstion from "express-session";
 import rootRouter from "./router/rootRouter";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const morganMiddleware = morgan("dev");
@@ -11,6 +13,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(morganMiddleware);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  sesstion({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
