28080962b28db3d57130d698e0567a60217b0e46
Seongmo Gu <gusm96@naver.com>
2021-05-26 17:05:54 +0900

    21.05.26 update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import sesstion from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./router/rootRouter";
 import userRouter from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
@@ -16,9 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   sesstion({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
