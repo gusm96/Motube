521b0725b4aa1f22c432ead0506cf5523e5caf51
Seongmo Gu <gusm96@naver.com>
2021-07-05 03:06:45 +0900

    video fix

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -73,8 +73,8 @@ export const postUpload = async (req, res) => {
   try {
     const newVideo = await Video.create({
       title,
-      fileUrl: ifHeroku ? video[0].location : video[0].path,
-      thumbUrl: ifHeroku ? thumb[0].location : thumb[0].path,
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : thumb[0].path,
       description,
       hashtags: Video.formatHashtags(hashtags),
       owner: _id,
